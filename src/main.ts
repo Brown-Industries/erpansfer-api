372a413e983fb6e41c1a641a42d9ad01f5cba7fc
Paul Sites <pts211@gmail.com>
2022-06-04 15:29:00 -0500

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('ERPansfer')
+  .setDescription('The ERPansfer API creates interoperability between JobBOSS and external systems. This api is the entry point to the workflows for consumers external to the business.')
+  .setVersion('0.0.0-alpha')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
